e47c5b912802eec218a0203b7bbf298db14182e6
ShowyCone <alexrafael2003@gmail.com>
2025-07-07 22:57:25 -0400

    services section done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
+import ServicesSection from './components/ServicesSection'
 
 const Home = () => {
   return (
     <main>
       <Hero />
+      <ServicesSection />
     </main>
   )
 }
