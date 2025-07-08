c1280ab81f19c41c1b3a21ada554ca54c1a5a5c0
ShowyCone <alexrafael2003@gmail.com>
2025-07-07 23:49:22 -0400

    notification component done

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Header from './components/layout/Header'
 import Home from './pages/home/Home'
 import AboutUs from './pages/aboutus/AboutUs'
 import Discover from './pages/discover/Discover'
+import NotificationBanner from './components/ui/NotificationBanner'
 
 function App() {
   return (
     <>
+      <NotificationBanner />
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
