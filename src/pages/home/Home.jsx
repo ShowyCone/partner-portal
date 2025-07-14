3e9d2d32458b7c3a45d5795bce6b9141222cacc7
ShowyCone <alexrafael2003@gmail.com>
2025-07-13 23:13:58 -0400

    banner section done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './components/Hero'
 import ServicesSection from './components/ServicesSection'
 import ChooseYourTrack from './components/ChooseYourTrack'
+import BannerSection from './components/BannerSection'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <ServicesSection />
       <ChooseYourTrack />
+      <BannerSection />
     </main>
   )
 }
