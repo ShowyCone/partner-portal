091b8515352fe7f68bbddb917c596e2b12896f8a
ShowyCone <alexrafael2003@gmail.com>
2025-07-13 22:09:56 -0400

    choose your track section done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './components/Hero'
 import ServicesSection from './components/ServicesSection'
+import ChooseYourTrack from './components/ChooseYourTrack'
 
 const Home = () => {
   return (
     <main>
       <Hero />
       <ServicesSection />
+      <ChooseYourTrack />
     </main>
   )
 }
