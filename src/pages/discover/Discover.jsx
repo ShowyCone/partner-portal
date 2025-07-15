ea86220a5f80cc2d44c0dab3eabe730f162086f8
ShowyCone <alexrafael2003@gmail.com>
2025-07-15 14:42:00 -0400

    hero section - discover page

--- src/pages/discover/Discover.jsx
+++ src/pages/discover/Discover.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Hero from './components/Hero'
 
 const Discover = () => {
-  return <div>Discover</div>
+  return (
+    <main className='flex flex-col gap-10'>
+      <Hero />
+    </main>
+  )
 }
 
 export default Discover
